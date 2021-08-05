a81511966a6ecfbdac7abae208fcd2ea0f465df2
aniruddhasahaindia <aniruddhasahaindia@gmail.com>
2021-08-05 22:19:28 +0530

    Step2 upload

--- ZCAF/src/Automation/Extras.java
+++ ZCAF/src/Automation/Extras.java
@@ -12,7 +12,7 @@ public class Extras {
 	public static void main(String[] args)
 	{
 		//System.out.println(getCurrentTimeStamp());
-		System.out.println("test");
+		System.out.println("test33");
 	}
 
 	public boolean highlightElement(WebDriver driver,WebElement wl) {
